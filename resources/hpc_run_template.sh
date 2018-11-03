8259e409dbcf192e510fd5d98bc6c101a064a5be
Jordan Berg <jordanberg@Jordans-MacBook-Pro.local>
2018-11-03 08:43:23 -0600

    Added comment about htseq download

--- resources/hpc_run_template.sh
+++ resources/hpc_run_template.sh
@@ -7,6 +7,7 @@
 #Set up whatever package we need to run with
 #Requires system or local installs of these programs
 module load hisat2 samtools python3 fastx_toolkit fastqc picard plastid star bedtools deeptools plastid
+#This does not install htseq, needed for alignment and must manually install if HPC does not provide or ask them to add
 
 #set up the temporary directory
 SCRDIR=/scratch/general/lustre/$USER/$SLURM_JOBID
@@ -23,4 +24,4 @@ cd $SCRDIR/.
 
 #run script --- add directory of raw data
 #Using Qiagen Illumina 3' adaptor, but shouldn't need adaptor trimming anyways, just a placeholder for now
-ribopipe riboseq -i $SCRDIR/raw_calvin/ -o $SCRDIR/out_calvin/ -r yeast -e calvin_test_1 -s cccp_00m cccp_05m cccp_10m cccp_15m cccp_30m cccp_60m -p HISAT2 -a GATCGGAAGAGCACACGTCTGAACTCCAGTCAC --platform ILLUMINA 
+ribopipe riboseq -i $SCRDIR/raw_calvin/ -o $SCRDIR/out_calvin/ -r yeast -e calvin_test_1 -s cccp_00m cccp_05m cccp_10m cccp_15m cccp_30m cccp_60m -p HISAT2 -a GATCGGAAGAGCACACGTCTGAACTCCAGTCAC --platform ILLUMINA
