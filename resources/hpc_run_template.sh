96518b276bdb4e5de862f412e37706ee014ab684
Jordan Berg <jordanberg@Jordans-MacBook-Pro.local>
2018-11-03 21:27:29 -0600

    Added license information to resource scripts

--- resources/hpc_run_template.sh
+++ resources/hpc_run_template.sh
@@ -4,6 +4,25 @@
 #SBATCH -o slurmjob-%j
 #SBATCH --partition=kingspeak
 
+#RiboPipe
+#An assembly and analysis pipeline for sequencing data
+#alias: ripopipe
+
+#Copyright (C) 2018  Jordan A. Berg
+#jordan <dot> berg <at> biochem <dot> utah <dot> edu
+
+#This program is free software: you can redistribute it and/or modify it under
+#the terms of the GNU General Public License as published by the Free Software
+#Foundation, either version 3 of the License, or (at your option) any later
+#version.
+
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY
+#WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+#PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License along with
+#this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #Set up whatever package we need to run with
 #Requires system or local installs of these programs
 module load hisat2 samtools python3 fastx_toolkit fastqc picard plastid star bedtools deeptools plastid
--- resources/install_singularity.sh
+++ resources/install_singularity.sh
@@ -1,8 +1,27 @@
+#RiboPipe
+#An assembly and analysis pipeline for sequencing data
+#alias: ripopipe
+
+#Copyright (C) 2018  Jordan A. Berg
+#jordan <dot> berg <at> biochem <dot> utah <dot> edu
+
+#This program is free software: you can redistribute it and/or modify it under
+#the terms of the GNU General Public License as published by the Free Software
+#Foundation, either version 3 of the License, or (at your option) any later
+#version.
+
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY
+#WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+#PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License along with
+#this program.  If not, see <https://www.gnu.org/licenses/>.
+
 sudo apt-get update && sudo apt-get install -y \
     build-essential \
     libssl-dev \
     uuid-dev \
-    libgpgme11-dev \ 
+    libgpgme11-dev \
     mono-devel
 
 sudo apt-get install golang-go
