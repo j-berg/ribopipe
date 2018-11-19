d1f50d9082019ff0b8907ebc40fbea58ed4dccc6
Jordan Berg <jordanberg@Jordans-MacBook-Pro.local>
2018-11-19 15:20:25 -0700

    Update

--- docs/conf.py
+++ docs/conf.py
@@ -75,8 +75,8 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+html_theme = 'rtd'
+html_theme_path = ['themes']
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
@@ -86,7 +86,20 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['nstatic']
+html_static_path = ['_static']
+html_style = 'labibi.css'
+
+# A shorter title for the navigation bar.  Default is the same as html_title.
+html_short_title = project + " v" + release
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = 'ribopipe.png'
+
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = 'ribopipe.png'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -96,13 +109,13 @@ html_static_path = ['nstatic']
 # default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
 # 'searchbox.html']``.
 #
-# html_sidebars = {}
+# html_sidebars = {'index': ['sidebar.html', 'sourcelink.html', 'searchbox.html']}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ribopipedoc'
+htmlhelp_basename = 'ribopipe-docs'
 
 
 # -- Options for LaTeX output ------------------------------------------------
