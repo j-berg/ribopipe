cbabebedd20a83bae89babb41597d92332d5ce5a
Jordan Berg <j-berg@users.noreply.github.com>
2018-12-06 18:37:23 -0700

    Add files via upload

---
+++ resources/reference_building/make_refFlat.sh
@@ -0,0 +1,4 @@
+
+
+gtfToGenePred Saccharomyces_cerevisiae.R64-1-1.94.gtf test3.txt
+awk '{print $1 "\t" $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" $7 "\t" $8 "\t" $9 "\t" $10}' test2.txt > test2full.txt
