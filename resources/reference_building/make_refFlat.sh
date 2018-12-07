c8b73b9e78d7478106cfc4ac7df9cc00c35a734e
Jordan Berg <j-berg@users.noreply.github.com>
2018-12-07 15:38:55 -0700

    Add files via upload

--- resources/reference_building/make_refFlat.sh
+++ resources/reference_building/make_refFlat.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 
+GTF=$1
+REF=$2
 
-gtfToGenePred Saccharomyces_cerevisiae.R64-1-1.94.gtf test3.txt
-awk '{print $1 "\t" $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" $7 "\t" $8 "\t" $9 "\t" $10}' test2.txt > test2full.txt
+gtfToGenePred $GTF $REF.tmp
+awk '{print $1 "\t" $1 "\t" $2 "\t" $3 "\t" $4 "\t" $5 "\t" $6 "\t" $7 "\t" $8 "\t" $9 "\t" $10}' $REF.tmp > $REF
+rm $REF.tmp
