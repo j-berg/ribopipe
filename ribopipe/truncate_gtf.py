d13bd1d4d5bf0b8dc044a5d81d7ac2b9e4314695
j-berg <{ID}+{username}@users.noreply.github.com>
2018-12-12 12:28:42 -0700

    Working out operation issues

--- ribopipe/truncate_gtf.py
+++ ribopipe/truncate_gtf.py
@@ -48,26 +48,26 @@ MAIN
 def truncate(args_dict):
 
     #Import gtf reference file to
-    print("reading in reference")
+    print("reading in reference...")
     if str(args_dict['input']).endswith('.gtf'):
         df = pd.read_csv(args_dict['input'], sep="\t", header=None, comment='#', low_memory=False)
     else:
         sys.exit(1)
 
     #Remove rows where "gene_id \"Y" is not present
-    print("gathering coding genes")
+    print("gathering coding genes...")
     df_coding = df[df.iloc[:, 8].str.contains('protein_coding') == True]
 
     #Save to .gtf file (tsv)
-    print("saving coding genes to reference file")
+    print("saving coding genes to reference file...")
     df_coding.to_csv(str(args_dict['input'])[:-4] + "_coding.gtf",sep="\t",header=None, index=False, quoting=csv.QUOTE_NONE)
 
     #Truncate first 45 nt of mRNA fasta (yeast) -- but with just the mRNA as reference, will that lose data since ribosome will need 30 nt of match to bind?
-    print("deep copying coding genes")
+    print("deep copying coding genes...")
     df_coding_copy = df_coding.copy()
 
     print("multiprocessing reference chunks -- this may take a while...")
     data = parallelize(df_coding_copy, truncate_chunk)
 
-    print("saving truncated reference")
+    print("saving truncated reference...")
     data.to_csv(str(args_dict['input'])[:-4] + "_truncated.gtf",sep="\t",header=None, index=False, quoting=csv.QUOTE_NONE)
