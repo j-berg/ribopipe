aa2a32500cf4ef69bce50117a4109d6691a13498
j-berg <{ID}+{username}@users.noreply.github.com>
2018-12-11 17:13:04 -0700

    Major overhaul to truncate sub-module (apply function to chunks of reference in parallel)

--- ribopipe/truncate_gtf.py
+++ ribopipe/truncate_gtf.py
@@ -3,53 +3,70 @@ import numpy as np
 import csv
 import re
 pd.options.mode.chained_assignment = None
+from multiprocessing import cpu_count, Pool
 
+"""
+FUNCTIONS
+"""
+#Truncate 45 nt
+def truncate_chunk(df_copy):
+    df_copy['plus'] = df_copy[[2,3,4,6,8]].apply(lambda x:
+        (x[3] + 45) if x[2] == "exon" and x[3] + 45 <= x[4] and x[6] == "+" and "exon_number \"1\"" in x[8] else (
+        "delete_this" if x[2] == "exon" and x[3] + 45 > x[4] and x[6] == "+" and "exon_number \"1\"" in x[8] else x[3]),axis=1)
+
+    df_copy['minus'] = df_copy[[2,3,4,6,8]].apply(lambda x:
+        (x[4] - 45) if x[2] == "exon" and x[3] <= x[4] - 45 and x[6] == "-" and "exon_number \"1\"" in x[8] else (
+        "delete_this" if x[2] == "exon" and x[3] > x[4] - 45 and x[6] == "-" and "exon_number \"1\"" in x[8] else x[4]),axis=1)
+
+    #remove exon1s that are too short
+    df_copy = df_copy[~df_copy['plus'].isin(['delete_this'])]
+    df_copy = df_copy[~df_copy['minus'].isin(['delete_this'])]
+
+    #copy new coordinates back to original columns
+    df_copy[3] = df_copy['plus']
+    df_copy[4] = df_copy['minus']
+
+    #remove placeholder columns
+    df_copy = df_copy.drop(columns=['plus','minus'])
+    return df_copy
+
+def parallelize(data, func):
+    data_split = np.array_split(data, partitions)
+    pool = Pool(cores)
+    data = pd.concat(pool.map(func, data_split))
+    pool.close()
+    pool.join()
+    return data
+
+"""
+MAIN
+"""
 def truncate(args_dict):
+
+    cores = cpu_count() #Number of CPU cores on your system
+    partitions = cores #Define as many partitions as you want
+
     #Import gtf reference file to
+    print("reading in reference")
     if str(args_dict['input']).endswith('.gtf'):
         df = pd.read_csv(args_dict['input'], sep="\t", header=None, comment='#', low_memory=False)
     else:
         sys.exit(1)
 
-    #Check that every df.iloc[:,8] contains 'gene_id'
-    df_id = df.iloc[:, 8].str.contains('protein_coding')
-
     #Remove rows where "gene_id \"Y" is not present
+    print("gathering coding genes")
     df_coding = df[df.iloc[:, 8].str.contains('protein_coding') == True]
 
-    #See what fell out
-    df_noncoding = df[df.iloc[:, 8].str.contains('protein_coding') != True]
-
     #Save to .gtf file (tsv)
+    print("saving coding genes to reference file")
     df_coding.to_csv(str(args_dict['input'])[:-4] + "_coding.gtf",sep="\t",header=None, index=False, quoting=csv.QUOTE_NONE)
 
-
     #Truncate first 45 nt of mRNA fasta (yeast) -- but with just the mRNA as reference, will that lose data since ribosome will need 30 nt of match to bind?
-    df_copy = df_coding.copy() #Make a deep copy where original won't be mutated in next step
+    print("deep copying coding genes")
+    df_coding_copy = df_coding.copy()
 
-    #Truncate 45 nt
-    naughty_genes = []
+    print("multiprocessing reference chunks")
+    data = parallelize(df_coding_copy, truncate_chunk)
 
-    for index, row in df_copy.iterrows():
-        if "exon_number \"1\"" in row[8]:
-            if row[6] == "+":
-                if row[2] == "exon" and (row[3] + 45) <= row[4]:
-                    df_copy.loc[index, 3] = row[3] + 45
-                elif row[2] == "exon" and (row[3] + 45) > row[4]:
-                    naughty_genes.append(index)
-                else:
-                    pass
-
-
-            elif row[6] == "-":
-                if row[2] == "exon" and (row[4] - 45) >= row[3]:
-                    df_copy.loc[index, 4] = row[4] - 45
-                elif row[2] == "exon" and (row[4] - 45) < row[3]:
-                    naughty_genes.append(index)
-                else:
-                    pass
-
-    #delete all rows with that id in row[8]
-    df1 = df_copy.drop(naughty_genes)
-
-    df1.to_csv(str(args_dict['input'])[:-4] + "_coding_truncated.gtf",sep="\t",header=None, index=False, quoting=csv.QUOTE_NONE)
+    print("saving truncated reference")
+    data.to_csv(str(args_dict['input'])[:-4] + "_coding_truncated.gtf",sep="\t",header=None, index=False, quoting=csv.QUOTE_NONE)
